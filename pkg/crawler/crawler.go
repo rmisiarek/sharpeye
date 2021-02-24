37418a22fa029f167550fa968dc35b1dd238e26c
Rafał Misiarek <misiarek@gmail.com>
2021-02-24 17:12:32 +0100

    Feature to grab URLs recursively

--- pkg/crawler/crawler.go
+++ pkg/crawler/crawler.go
@@ -1,7 +1,10 @@
 package crawler
 
 import (
+	"net/http"
 	"sync"
+
+	"golang.org/x/net/html"
 )
 
 type urlCache struct {
@@ -34,5 +37,64 @@ func newResults() *results {
 	return &results{
 		data: make(chan string, 1),
 		err:  make(chan error, 1),
+	}
+}
+
+func grablUrls(resp *http.Response, node *html.Node) []string {
+	var f func(*html.Node) []string
+	var results []string
+
+	f = func(n *html.Node) []string {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue
+				}
+				results = append(results, link.String())
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+
+		return results
+	}
+
+	res := f(node)
+	return res
+}
+
+func crawl(url string, depth int, wg *sync.WaitGroup, cache *urlCache, res *results) {
+	defer wg.Done()
+
+	if depth == 0 || !cache.Set(url) {
+		return
+	}
+
+	response, err := http.Get(url)
+	if err != nil {
+		res.err <- err
+		return
+	}
+	defer response.Body.Close()
+
+	node, err := html.Parse(response.Body)
+	if err != nil {
+		res.err <- err
+		return
+	}
+
+	urls := grablUrls(response, node)
+
+	res.data <- url
+
+	for _, url := range urls {
+		wg.Add(1)
+		go crawl(url, depth-1, wg, cache, res)
 	}
 }
