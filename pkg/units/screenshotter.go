6e51bffd867aae26dcd459490e4eb7920069dfaf
Rafał Misiarek <misiarek@gmail.com>
2021-03-21 18:40:58 +0100

    Pick solution without chromedp

--- pkg/units/screenshotter.go
+++ pkg/units/screenshotter.go
@@ -4,91 +4,17 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os/exec"
 	"time"
-
-	"github.com/chromedp/cdproto/emulation"
-	"github.com/chromedp/cdproto/page"
-	"github.com/chromedp/chromedp"
 )
 
-// TakeScreenshot ...
-func TakeScreenshot(url string, id int) (*[]byte, error) {
-	start := time.Now()
-	fmt.Println("Start TakeScreenshot - ", start)
-	options := []chromedp.ExecAllocatorOption{}
-	options = append(options, chromedp.DefaultExecAllocatorOptions[:]...)
-	// options = append(options, chromedp.UserAgent(chrome.UserAgent))
-	// options = append(options, chromedp.DisableGPU)
-	// options = append(options, chromedp.Headless)
-	// options = append(options, chromedp.NoSandbox)
-	// options = append(options, chromedp.NoFirstRun)
-	// options = append(options, chromedp.NoDefaultBrowserCheck)
-	options = append(options, chromedp.WindowSize(1280, 960))
-	options = append(options, chromedp.Flag("ignore-certificate-errors", true))
+// TODO: read from config or just try to find it
+var chromePath = "/usr/bin/google-chrome-stable"
 
-	actx, acancel := chromedp.NewExecAllocator(context.Background(), options...)
-	ctx, cancel := chromedp.NewContext(actx)
-	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
-
-	defer acancel()
-	defer cancel()
-
-	var buf []byte
-
-	if err := chromedp.Run(ctx, fullScreenshot(url, 1280, 960, 50, &buf)); err != nil {
-		log.Println("chromedp.Run: ", err)
-	}
-
-	path := "/home/raf/go/src/sharpeye/screens3/image_" + fmt.Sprint(id) + ".png"
-	err := ioutil.WriteFile(path, buf, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	elapsed := time.Since(start)
-	log.Printf("TakeScreenshot took %s", elapsed)
-
-	return &buf, nil
-}
-
-func fullScreenshot(
-	urlstr string, width int64, height int64, quality int64, res *[]byte,
-) chromedp.Tasks {
-
-	return chromedp.Tasks{
-		chromedp.Navigate(urlstr),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			err := emulation.SetDeviceMetricsOverride(width, height, 1, false).
-				WithScreenOrientation(&emulation.ScreenOrientation{
-					Type:  emulation.OrientationTypePortraitPrimary,
-					Angle: 0,
-				}).Do(ctx)
-
-			if err != nil {
-				return err
-			}
-
-			*res, err = page.CaptureScreenshot().
-				WithQuality(quality).
-				WithClip(&page.Viewport{
-					Width:  float64(width),
-					Height: float64(height),
-					Scale:  1,
-				}).Do(ctx)
-
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}),
-	}
-}
-
-func TakeScr(url string, id int) (string, error) {
+// TakeScreenshot takes a screenshot of the website given as a parameter.
+// It returns string with path to saved screenshot if succeed or empty
+// string when it fails.
+func TakeScreenshot(url string, id int) (string, error) {
 	screenshotFilePath := screenshotFilePath(id)
 
 	var chromeOpts = []string{
@@ -116,7 +42,6 @@ func TakeScr(url string, id int) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(30*time.Second))
 	defer cancel()
 
-	chromePath := "/usr/bin/google-chrome-stable"
 	cmd := exec.CommandContext(ctx, chromePath, chromeOpts...)
 	if err := cmd.Start(); err != nil {
 		killCmd(cmd)
