d5cc6358d6a9ca776194a88d11811100d458bd7f
Rafał Misiarek <misiarek@gmail.com>
2021-03-21 13:24:38 +0100

    Approach to take screenshots without chromedp

--- pkg/units/screenshotter.go
+++ pkg/units/screenshotter.go
@@ -2,7 +2,11 @@ package units
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os/exec"
 	"time"
 
 	"github.com/chromedp/cdproto/emulation"
@@ -13,7 +17,7 @@ import (
 // TakeScreenshot ...
 func TakeScreenshot(url string, id int) (*[]byte, error) {
 	start := time.Now()
-
+	fmt.Println("Start TakeScreenshot - ", start)
 	options := []chromedp.ExecAllocatorOption{}
 	options = append(options, chromedp.DefaultExecAllocatorOptions[:]...)
 	// options = append(options, chromedp.UserAgent(chrome.UserAgent))
@@ -27,6 +31,7 @@ func TakeScreenshot(url string, id int) (*[]byte, error) {
 
 	actx, acancel := chromedp.NewExecAllocator(context.Background(), options...)
 	ctx, cancel := chromedp.NewContext(actx)
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
 
 	defer acancel()
 	defer cancel()
@@ -35,6 +40,12 @@ func TakeScreenshot(url string, id int) (*[]byte, error) {
 
 	if err := chromedp.Run(ctx, fullScreenshot(url, 1280, 960, 50, &buf)); err != nil {
 		log.Println("chromedp.Run: ", err)
+	}
+
+	path := "/home/raf/go/src/sharpeye/screens3/image_" + fmt.Sprint(id) + ".png"
+	err := ioutil.WriteFile(path, buf, 0644)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	elapsed := time.Since(start)
@@ -75,4 +86,61 @@ func fullScreenshot(
 			return nil
 		}),
 	}
+}
+
+func TakeScr(url string, id int) (string, error) {
+	screenshotFilePath := screenshotFilePath(id)
+
+	var chromeOpts = []string{
+		"--headless",
+		"--no-sandbox", // docker
+		"--disable-gpu",
+		"--incognito",
+		"--no-first-run",
+		"--mute-audio",
+		"--hide-scrollbars",
+		"--disable-notifications",
+		"--disable-crash-reporter",
+		"--ignore-certificate-errors",
+		"--no-default-browser-check",
+		"--user-data-dir=screens3", // TODO: change it
+		"--disable-infobars",
+		"--disable-sync",
+		// "--user-agent=",
+		"--window-size=1280,960",
+		"--screenshot=" + screenshotFilePath,
+	}
+
+	chromeOpts = append(chromeOpts, url)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(30*time.Second))
+	defer cancel()
+
+	chromePath := "/usr/bin/google-chrome-stable"
+	cmd := exec.CommandContext(ctx, chromePath, chromeOpts...)
+	if err := cmd.Start(); err != nil {
+		killCmd(cmd)
+		return "", errors.New("in cmd.Start()")
+	}
+
+	if err := cmd.Wait(); err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			killCmd(cmd)
+			return "", errors.New("timed out")
+		}
+		killCmd(cmd)
+		return "", errors.New("in cmd.Wait()")
+	}
+
+	killCmd(cmd)
+	return screenshotFilePath, nil
+}
+
+func killCmd(cmd *exec.Cmd) {
+	cmd.Process.Release()
+	cmd.Process.Kill()
+}
+
+func screenshotFilePath(id int) string {
+	return "screens3/image_" + fmt.Sprint(id) + ".png"
 }
