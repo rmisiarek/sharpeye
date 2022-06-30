a3dce18a9d744098cf637c748c8070902f895a50
Rafal Misiarek <misiarek@gmail.com>
2022-06-30 22:43:46 +0200

    Add customized http client (#3)

---
+++ sharpeye/client.go
@@ -0,0 +1,98 @@
+package sharpeye
+
+import (
+	"crypto/tls"
+	"net"
+	"net/http"
+	"time"
+)
+
+type tlsResponse struct {
+	version    uint16
+	serverName string
+}
+
+type response struct {
+	statusCode int
+	url        string
+	headers    map[string][]string
+}
+
+type HttpClient struct {
+	client *http.Client
+}
+
+func newHttpClient(followRedirect bool, timeout int) *HttpClient {
+	var tlsConfig = &tls.Config{InsecureSkipVerify: true}
+
+	var dialContext = &net.Dialer{
+		Timeout:   time.Second * 5,
+		KeepAlive: time.Second,
+	}
+
+	var transport = &http.Transport{
+		MaxIdleConns:      30,
+		IdleConnTimeout:   time.Second,
+		DisableKeepAlives: true,
+		TLSClientConfig:   tlsConfig,
+		DialContext:       dialContext.DialContext,
+	}
+
+	client := &http.Client{
+		Transport: transport,
+		Timeout:   time.Duration(timeout) * time.Second,
+	}
+
+	if followRedirect {
+		client.CheckRedirect = nil
+	} else {
+		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+	}
+
+	return &HttpClient{client: client}
+}
+
+func (h *HttpClient) request(url string, method string, headers http.Header) (response, tlsResponse, error) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return response{}, tlsResponse{}, err
+	}
+
+	if len(headers) != 0 {
+		req.Header = headers
+	}
+
+	req.Header.Add("Connection", "close")
+	req.Close = true
+
+	resp, err := h.client.Do(req)
+	if err != nil {
+		return response{}, tlsResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	var tls tlsResponse
+	if resp.TLS != nil {
+		tls = tlsResponse{
+			version:    resp.TLS.Version,
+			serverName: resp.TLS.ServerName,
+		}
+	}
+
+	return response{
+		statusCode: resp.StatusCode,
+		url:        resp.Request.URL.String(),
+		headers:    resp.Header,
+	}, tls, nil
+}
+
+// func getLocation(locationF func() (*url.URL, error)) string {
+// 	location, err := locationF()
+// 	if location == nil || err != nil {
+// 		return ""
+// 	} else {
+// 		return location.String()
+// 	}
+// }
